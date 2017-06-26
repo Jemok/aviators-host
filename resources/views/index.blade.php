3fa3f225a070ee5b02c32ddc358fe1f24b707ac7
James Mwangi <karokijames40@gmail.com>
2017-06-26 17:09:48 +0300

    More changes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,4 +3,9 @@
 @include('layouts.nav')
 
 
+<div class="container-fluid landing-image">
+    <img src="{{ asset('images/aviators-landing.jpg') }}" 	width="699px" height="349px">
+</div>
+
+
 @include('layouts.footer')
