511b0a86d11bb0e79dbd812643615d9a1bee5b08
James Mwangi <karokijames40@gmail.com>
2017-06-26 16:50:01 +0300

    Adds header complete

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -8,52 +8,20 @@
     <title></title>
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
-    <style>
-        .header{
-            display: flex;
-            margin-top: 2em;
-            margin-left: 85%;
-            height: 4em;
-            justify-content: space-around;
-        }
-        .footer{
-            width: 100%;
-            background-color: #1C2B39;
-            height: 20em;
-            color: white;
-            display: flex;
-        }
-        .content{
-          text-align: center;
-            margin:auto;
-            padding: 1em;
-        }
-        .content_two{
-            background-color: lightgray;
-            padding: 2em;
-            display: flex;
-        }
-        .left {
-            display: flex;
-        }
-        .item {
-            display: flex;
-            list-style: none;
-        }
-
-        .right {
-            text-align: right;
-            margin-left:50%;
-        }
-
-    </style>
 </head>
 <body>
-<div class="header">
-    <i class="fa fa-facebook-official fa-3x" aria-hidden="true"></i>
-    <i class="fa fa-instagram fa-3x" aria-hidden="true"></i>
-    <i class="fa fa-twitter-square fa-3x" aria-hidden="true"></i>
+<div class="container-fluid">
+    <div class="row">
+        <div class="pull-left logo">
+            <img src="{{ asset('images/logo.svg')  }}" width="178" height="75" alt="" data-mu-svgfallback="images/logo_poster_.png"/>
+        </div>
+        <div class="pull-right social-icons">
+            <i class="fa fa-facebook-official fa-3x" aria-hidden="true"></i>
+            <i class="fa fa-instagram fa-3x" aria-hidden="true"></i>
+            <i class="fa fa-twitter-square fa-3x" aria-hidden="true"></i>
+        </div>
+    </div>
 </div>
